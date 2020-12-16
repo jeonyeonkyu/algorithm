587ac73583bed134a3b6e9e7db8b8d05e45b8919
yeonkyujeon <ehb6915@naver.com>
2020-12-16 12:12:50 +0900

    Fix : getArray따로 함수 분리, getArray는 재귀함수로 구현

--- leetcode/challenge-569-2.js
+++ leetcode/challenge-569-2.js
@@ -1,3 +1,5 @@
+//https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/569/week-1-december-1st-december-7th/3552/
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -10,25 +12,24 @@
         Note that the head is guaranteed to be not null, so it contains at least one node.
  * @param {ListNode} head
  */
-var Solution = function(head) {    
-  this.count = 0;
-  this.head = head;
-   this.array = [];
-  if(this.head){
-      this.array.push(this.head.val)
-      this.count++;
-      while(this.head.next){
-         this.head = this.head.next;
-          this.array.push(this.head.val)
-          this.count++;
-      }
-  }
+var Solution = function (head) {
+  this.array = this.getArray(head);
 };
 
 /**
 * Returns a random node's value.
 * @return {number}
 */
-Solution.prototype.getRandom = function() {
-  return this.array[Math.floor(Math.random()*this.count)];
+Solution.prototype.getRandom = function () {
+  return this.array[Math.floor(Math.random() * this.array.length)];
 };
+
+Solution.prototype.getArray = function (root, array = []) {
+  if (!root) {
+    return array;
+  } else {
+    array.push(root.val)
+    Solution.prototype.getArray(root.next, array)
+  }
+  return array
+}
